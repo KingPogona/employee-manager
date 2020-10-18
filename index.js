c0f4435148a2e64853bf75f5f8b80a45b0342708
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-10-17 20:34:15 -0600

    add basic set of queries for project

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+// const { connection, endConnection } = require('./lib/queries.js');
+const queries = require('./lib/queries.js');
+
