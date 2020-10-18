4166583e43ed88ffaf504d9d10042801d2cae813
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-10-18 14:26:19 -0600

    add prompts

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+// const cTable = require('console.table');
 
 // const { connection, endConnection } = require('./lib/queries.js');
-const queries = require('./lib/queries.js');
+// const queries = require('./lib/queries.js');
+const Prompts = require('./lib/prompts.js');
 
+new Prompts().initializeProgram();
